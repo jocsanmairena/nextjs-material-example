105721aeaea7702ff75ae313fe2283712349d54b
Jocsan Mairena <jocsan.mairena@gmail.com>
2020-10-02 16:47:39 -0400

    boilerplate setup for Material UI Theming

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import Header from './ui/Header'
-
+import { ThemeProvider } from '@material-ui/styles'
+import theme from './ui/Theme'
 const App = () => (
   <>
-    <div className='App'>
-      <Header>
-        <h1>This is my header</h1>
-      </Header>
-      {[...new Array(300)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join('\n')}
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header>This is my header</Header>
+    </ThemeProvider>
   </>
 )
 
